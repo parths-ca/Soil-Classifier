fcdd8f433d0cabb5b67b2fffccdb45cbbf084ac7
parthshahca468@gmail.com <parthshahca468@gmail.com>
2021-04-19 13:22:45 -0400

    Publish

---
+++ Deployment/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import cv2
+from PIL import Image
+import numpy as np
+from keras.models import load_model
+
+
+def init():
+    """Load Model and Start Classifier System"""
+    global model
+    model = load_model('model/trainedModel.h5')
+
+
+def preprocess_image(img):
+    """Preprocess uploaded image before sending for prediction"""
+    # convert the pillow image array into a numpy array
+    img = np.array(img)
+    # transform the array to BGR format of cv2 images
+    img = img[:, :, ::-1].copy()
+    # resize the image to input size of model
+    img = cv2.resize(img, (32, 32))
+    # expand dimensions to ensure model input dimensions
+    return np.expand_dims(img, axis=0)
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/SoilClassifier')
+def startApp():
+    """Offer Front page of application to the user"""
+    return render_template('index.html')
+
+
+@app.route('/getResult', methods=['POST'])
+def getResult():
+    """Uses the uploaded image to predict soil type and return result"""
+    if request.method == 'POST':
+        soil_image = Image.open(request.files['image'].stream).convert('RGB')
+        soil_image = preprocess_image(soil_image)
+        result_val = np.argmax(model.predict([soil_image])).item()
+        result = {"result": result_val, "status": 200}
+        return result
+    return {"status": 500}
+
+if __name__ == '__main__':
+    init()
+    app.run(host='0.0.0.0', port=4567)
